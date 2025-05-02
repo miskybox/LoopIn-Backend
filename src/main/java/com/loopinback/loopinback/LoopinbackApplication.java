b6d2c9f12aec1da6333505b8ebf0011b6d73cfa9
SiGu <miskybox@gmail.com>
2025-05-02 13:02:31 +0200

    Initial commit -FEAT- Files .env, application.properties

---
+++ src/main/java/com/loopinback/loopinback/LoopinbackApplication.java
@@ -0,0 +1,13 @@
+package com.loopinback.loopinback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoopinbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoopinbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loopinback/loopinback/LoopinbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loopinback.loopinback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopinbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
