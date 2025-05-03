0c668ad766b6837b46a80a843694bc4326dbf302
SiGu <miskybox@gmail.com>
2025-05-03 12:14:56 +0200

    FEAT-folders and files Exceptions

---
+++ src/main/java/com/loopinback/loopinback/exception/ErrorConstants.java
@@ -0,0 +1,15 @@
+package com.loopinback.loopinback.exception;
+
+public class ErrorConstants {
+    private ErrorConstants() {
+
+    }
+
+    public static final String TIMESTAMP = "timestamp";
+    public static final String MESSAGE = "message";
+    public static final String DETAILS = "details";
+    public static final String USER_NOT_FOUND = "Usuario no encontrado";
+    public static final String EVENT_NOT_FOUND = "Evento no encontrado";
+    public static final String ATTENDANCE_NOT_FOUND = "Asistencia no encontrada";
+    public static final String TICKET_NOT_FOUND = "Ticket no encontrado";
+}
