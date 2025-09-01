c9dee7dbc09020d999c656fe2901e76d34009280
SiGu <miskybox@gmail.com>
2025-09-01 16:51:26 +0200

    FEAT-CorsConfig

--- src/main/java/com/loopinback/loopinback/config/CorsConfig.java
+++ src/main/java/com/loopinback/loopinback/config/CorsConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@org.springframework.lang.NonNull CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
